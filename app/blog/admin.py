113e0c808f11f0319f4cafb5016d75c5e1c4ab69
asman <asmanmalikov@gmail.com>
2018-05-10 15:17:53 +0300

    added github

---
+++ app/blog/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Category, Tags, Article
+
+
+# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    pass
---
+++ app/blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from core.models import TimeStampedModel
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+class Category(TimeStampedModel):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Tags(TimeStampedModel):
+    tag = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.tag
+
+
+class Article(TimeStampedModel):
+    title = models.CharField(max_length=200)
+    short_text = models.CharField('Description', max_length=200)
+    text = models.TextField('Article')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    Tags = models.ManyToManyField(Tags, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
